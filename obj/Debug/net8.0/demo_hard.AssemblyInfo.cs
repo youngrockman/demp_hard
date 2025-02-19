a22617f76bc998c4e3da55d9c7e9f6e7477c1a11
-SSS- <karpoff0502@gmail.com>
2025-02-19 11:25:02 +0300

    init commit

--- obj/Debug/net8.0/demo_hard.AssemblyInfo.cs
+++ obj/Debug/net8.0/demo_hard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("demo_hard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a24d02ba9bcc9b996f261baa314bdc3640928f7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0421119ccc3a8fb4511ce1afcf4ded03661d4361")]
 [assembly: System.Reflection.AssemblyProductAttribute("demo_hard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("demo_hard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
